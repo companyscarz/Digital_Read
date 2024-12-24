632d3dba8bf37a0e29a7a17eb684ff87839e9b38
Company ScarZ <161337792+companyscarz@users.noreply.github.com>
2024-12-24 11:04:48 +0300

    Create delicate.py
    created the stuff for the database

---
+++ delicate.py
@@ -0,0 +1,6 @@
+import psycopg2
+conn = psycopg2.connect(database = "datacamp_courses", 
+                        user = "datacamp", 
+                        host= 'localhost',
+                        password = "postgresql_tutorial",
+                        port = 5432)
