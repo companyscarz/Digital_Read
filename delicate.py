c6e3a573e7c8437bb1b35ab60d4da3f8a2d180f3
Company ScarZ <161337792+companyscarz@users.noreply.github.com>
2024-12-25 01:03:01 +0300

    Update delicate.py

--- delicate.py
+++ delicate.py
@@ -3,6 +3,5 @@ connection = psycopg2.connect(database = "d6td5p7sln9dgt",
                         user = "uglknncdkf1j2", 
                         host= 'ccaml3dimis7eh.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com',
                         password = "pe1d707020029661359609189274a66c335f1d9149614aef42d0ac235bbb634f7",
-                        DATABASE_URL = "postgres://uglknncdkf1j2:pe1d707020029661359609189274a66c335f1d9149614aef42d0ac235bbb634f7@ccaml3dimis7eh.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d6td5p7sln9dgt"
                         port = 5432
                              )
