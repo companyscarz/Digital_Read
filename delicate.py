6a7a4fba7c02adb1888cb94c724a5c1624b42b58
Company ScarZ <161337792+companyscarz@users.noreply.github.com>
2024-12-24 11:07:20 +0300

    Update delicate.py

--- delicate.py
+++ delicate.py
@@ -1,6 +1,6 @@
 import psycopg2
-conn = psycopg2.connect(database = "datacamp_courses", 
-                        user = "datacamp", 
-                        host= 'localhost',
-                        password = "postgresql_tutorial",
+conn = psycopg2.connect(database = "d6td5p7sln9dgt", 
+                        user = "uglknncdkf1j2", 
+                        host= 'ccaml3dimis7eh.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com',
+                        password = "pe1d707020029661359609189274a66c335f1d9149614aef42d0ac235bbb634f7",
                         port = 5432)
