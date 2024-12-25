09425b1be85d9654a1b69939f7176936a200d45e
Company ScarZ <161337792+companyscarz@users.noreply.github.com>
2024-12-25 20:09:58 +0300

    Update delicate.py

--- delicate.py
+++ delicate.py
@@ -1,7 +1,7 @@
-import psycopg2
-connection = psycopg2.connect(database = "d6td5p7sln9dgt", 
-                        user = "uglknncdkf1j2", 
-                        host= 'ccaml3dimis7eh.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com',
-                        password = "pe1d707020029661359609189274a66c335f1d9149614aef42d0ac235bbb634f7",
-                        port = 5432
-                             )
+import pg8000
+connection = pg8000.connect(
+    host="ccaml3dimis7eh.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com",  # Replace with your PostgreSQL host
+    user="uglknncdkf1j2",  # Replace with your username
+    password="pe1d707020029661359609189274a66c335f1d9149614aef42d0ac235bbb634f7",  # Replace with your password
+    database="d6td5p7sln9dgt"  # Replace with your database name
+)
