f915d4bcd646d5b1445ce4d59bce822b332df28f
Company ScarZ <161337792+companyscarz@users.noreply.github.com>
2024-12-26 15:43:01 +0300

    Update view.py

--- Digital_read/view.py
+++ Digital_read/view.py
@@ -2,6 +2,7 @@ from flask import redirect, url_for, flash, render_template, request, Blueprint,
 from models.view_models import cleanup_online_tb
 from settings import Config
 from Digital_read.card_stock import magazine_stocks, math_stocks
+from apscheduler.schedulers.background import BackgroundScheduler
 
 view_bp = Blueprint('view_bp', __name__, template_folder='templates/digital_read', static_folder='static')
 
@@ -14,7 +15,12 @@ def View():
     active_cardcode = session.get('card_code')  # Extract card code from session
     
     #return render_template('view.html', title=title)
-    cleanup_online_tb()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(cleanup_online_tb, 'interval', minutes=1)
+    scheduler.start()
+    print ('online db cleared')
+    #cleanup_online_tb()
+    
     if 'card_num' and 'card_code' in session:
     	if active_cardnum in magazine_stocks and magazine_stocks[active_cardnum]== active_cardcode:
     		#flash("Welcome to the digital magazine!")
