da5878f1bd474cd35bf21466d87a76b72e3afb92
Company ScarZ <161337792+companyscarz@users.noreply.github.com>
2024-12-30 14:50:31 +0300

    Update authorisation.py

--- Digital_read/authorisation.py
+++ Digital_read/authorisation.py
@@ -12,16 +12,10 @@ def Authorisation():
     form = Verification()
     current_time = datetime.utcnow()#get current time
     if form.validate_on_submit():
-    	#check if the card number is in the list of card numbers... 
-    	#this ensures only specific cards can continue..
-    	#then check if card is on online_tb so that only one person can use it
-    	# then continue to look for... 
+        if form.card_number.data in magazine_stocks and magazine_stocks[active_cardnum]== active_cardcode:
     	data = CArds(form.card_number.data, form.password.data, current_time)
-    	return (data.Look_up())
-    	#the Look_up function checks if the card number is in the database 
-    	#if true then saves the card number and password as sesions and adds the card to online table
-    	#if not in db then it adds on the cards db and then adds the card to online table and saves the card and password on session
-    	#return redirect(url_for('view_bp.View'))
+    	data.Look_up()
+        return redirect(url_for("view_bp.View")
     else:
     	flash('Please fill in the fields correctly!!')
     	return render_template('authorisation.html', title=title, form=form)
