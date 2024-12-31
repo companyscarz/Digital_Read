df8c7b28d30a4b100948773a932f2b6ed947a0fb
Company ScarZ <161337792+companyscarz@users.noreply.github.com>
2024-12-31 18:14:56 +0300

    Update authorisation.py

--- Digital_read/authorisation.py
+++ Digital_read/authorisation.py
@@ -18,8 +18,7 @@ def Authorisation():
         if form.card_number.data in magazine_stocks and magazine_stocks[form.card_number.data]== form.password.data:
             data = CArds(form.card_number.data, form.password.data, current_time)
             data.Look_up()
-            pass
-            #return redirect(url_for("view_bp.View"))
+            return render_template('authorisation.html', title=title, form=form)
         else:
             flash("Unknown card!")
             return render_template('authorisation.html', title=title, form=form)
