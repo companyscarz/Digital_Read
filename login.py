176b3c85695ea380f68659f3548f01ab68bc044b
Company ScarZ <161337792+companyscarz@users.noreply.github.com>
2025-05-08 21:22:00 +0300

    Delete login.py

--- login.py
+++
@@ -1,9 +0,0 @@
-from flask import redirect, url_for, flash, render_template, request, Blueprint, session
-from settings import Config
-
-login_bp = Blueprint('login_bp', __name__, template_folder='templates/digital_read', static_folder='static')
-
-@login_bp.route('/login')
-def Login():
-    title="login"
-    return render_template('login.html', title=title)
